95a49e0558ff0564e7af3200a5f6eeef393e2a96
dhardestylewis <danielhardestylewis@utexas.edu>
2021-09-22 14:09:50 -0500

    testing no to_crs speed

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import geopandas as gpd
 
 
 gdf = gpd.read_file('https://gist.githubusercontent.com/Tlaloc-Es/5c82834e5e4a9019a91123cb11f598c0/raw/709ce9126861ef7a7c7cc4afd6216a6750d4bbe1/mexico.geojson')
-gdf = gdf.to_crs(epsg=4326)
+#gdf = gdf.to_crs(epsg=4326)
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
